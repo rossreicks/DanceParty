0a422704a3fcda11264fb98c44a4fa06280c3011
Ross Reicks <rreicks13@gmail.com>
2020-06-28 12:23:55 -0500

    added ws env to prod file

--- sites/dance-party/src/environments/environment.prod.ts
+++ sites/dance-party/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  spotify_redirect_url: 'https://dance-party.rossreicks.com/owner'
+  spotify_redirect_url: 'https://dance-party.rossreicks.com/owner',
+  ws: 'wss://8o864yqzkc.execute-api.us-east-1.amazonaws.com/Prod'
 };
